aeeb4fab9a69afa6ae38478e49c27235a68ce023
1223Bb <brunoscachetti@hotmail.com>
2022-09-05 18:39:12 -0300

    Player Movement script created
    Each script will take care of it's own thing, keeps things organized and modular, though everything will be controlled by a main script, for now called "Logic" script.

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        GetReferences();
+    }
+
+    private void GetReferences()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void MovePlayer(Vector2 direction)
+    {
+        Vector3 desiredPosition;
+        Vector3 vec3Ddirection = new Vector3(direction.x, direction.y, 0);
+        desiredPosition = this.transform.position + vec3Ddirection;
+        rb.MovePosition(desiredPosition);
+    }
+}
